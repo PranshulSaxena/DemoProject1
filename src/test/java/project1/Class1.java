fda9e33306acabbfe6f95becfc8c6ad0b0cff302
PranshulSaxena <saxenap745@gmail.com>
2022-11-23 16:05:25 +0530

    c1

--- src/test/java/project1/Class1.java
+++ src/test/java/project1/Class1.java
@@ -1,11 +1,17 @@
 package project1;
 
+import org.testng.annotations.Test;
+
 public class Class1 {
 
-	public static void main(String[] args) {
+	@Test
+	public void class1() {
 		System.out.println("this is demo 1");
 		
 		System.out.println("this is demo 2");
+		
+		
+		
 		
 		
 
